97c364c80d390bc59e6e16b2b882ccc650feaf20
Martynas Asipauskas <me@martynas.co.uk>
2017-01-21 01:12:01 +0000

    Still few bugs remaining in the solver

--- progfun2/streams/src/main/scala/streams/Solver.scala
+++ progfun2/streams/src/main/scala/streams/Solver.scala
@@ -76,18 +76,30 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = for {
+    (block, history) <- initial
+    unexplored = newNeighborsOnly(neighborsWithHistory(block, history), explored)
+    (block, _) <- unexplored
+    nextMove <- from(unexplored, Set(block) ++ explored)
+  } yield nextMove
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(Stream((startBlock, List.empty)), Set.empty)
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = {
+    def blockReachedGoal(pair: (Block, List[Move])) = {
+      val (block, _) = pair
+      done(block)
+    }
+
+    pathsFromStart.filter(blockReachedGoal)
+  }
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -97,5 +109,12 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = {
+    def moves(pair: (Block, List[Move])) = {
+      val (_, movesToBlock) = pair
+      movesToBlock
+    }
+
+    pathsToGoal.headOption.toList.flatMap(moves)
+  }
 }
--- progfun2/streams/src/main/scala/streams/StringParserTerrain.scala
+++ progfun2/streams/src/main/scala/streams/StringParserTerrain.scala
@@ -52,7 +52,14 @@ trait StringParserTerrain extends GameDef {
    * a valid position (not a '-' character) inside the terrain described
    * by `levelVector`.
    */
-  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = (p: Pos) => levelVector(p.row)(p.col) != '-'
+  def terrainFunction(levelVector: Vector[Vector[Char]]): Pos => Boolean = (p: Pos) => {
+    def withinBounds(p: Pos) = {
+      def inRange(value: Int, size: Int) = value >= 0 && value < size
+      inRange(p.row, levelVector.size) && inRange(p.col, levelVector.headOption.map(_.size).getOrElse(0))
+    }
+    withinBounds(p) && levelVector(p.row)(p.col) != '-'
+  }
+
 
   /**
    * This function should return the position of character `c` in the
