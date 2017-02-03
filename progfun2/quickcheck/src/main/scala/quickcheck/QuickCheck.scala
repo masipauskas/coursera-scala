aff8a59fc08a82df3006e88c1e49ac070b28a10e
Martynas Asipauskas <me@martynas.co.uk>
2017-02-03 18:45:15 +0000

    Week 3 - QuickCheck exercise completed

--- progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -1,14 +1,12 @@
 package quickcheck
 
 import common._
-
 import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
-
   lazy val genHeap: Gen[H] = for {
     value <- arbitrary[A]
     heap <- oneOf(const(empty), genHeap)
@@ -16,7 +14,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { (h: H) =>
+  property("findMinIfMinIsInsertedShouldBeTheSame") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
@@ -31,14 +29,6 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
   }
 
   property("findingAndRemovingTheMinimumOfTheHeapShouldReturnSortedListOfElements") = forAll { (heap: H) =>
-    def elements(h: H, acc: List[A]): List[A] = {
-      if (isEmpty(h)) acc
-      else {
-        val min = findMin(h)
-        elements(deleteMin(h), min :: acc)
-      }
-    }
-
     val all = elements(heap, List.empty)
     all.sorted == all.reverse
   }
@@ -54,6 +44,26 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
       val min = if (firstMin < secondMin) firstMin else secondMin
 
       findMin(meld(first, second)) == min
+    }
+  }
+
+  property("creatingHeapFromArbitraryListOfNumbersAndThenGettingElementsShouldReturnAllElementsInCorrectOrder") = forAll(nonEmptyListOf(arbitrary[A])) { (elementsToInsert: List[A]) =>
+    def makeHeap(e: List[A], h: H): H = {
+      if (e.isEmpty) h
+      else makeHeap(e.tail, insert(e.head, h))
+    }
+
+    val sorted = elementsToInsert.sorted
+    val heap = makeHeap(elementsToInsert, empty)
+
+    sorted == elements(heap, List.empty).reverse
+  }
+
+  def elements(h: H, acc: List[A]): List[A] = {
+    if (isEmpty(h)) acc
+    else {
+      val min = findMin(h)
+      elements(deleteMin(h), min :: acc)
     }
   }
 }
