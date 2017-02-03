a0ab165c992d8b6308cbafab457b2a3cee0d2200
Martynas Asipauskas <me@martynas.co.uk>
2017-02-03 18:06:46 +0000

    Added property checks to validate the heap

--- progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ progfun2/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -21,4 +21,39 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  property("insertAndRemoveShouldResultInEmptyHeap") = forAll { (value: A) =>
+    deleteMin(insert(value, empty)) == empty
+  }
+
+  property("insertTwoElementsIntoTheHeapFindMinShouldFindSmallestElement") = forAll { (first: A, second: A) =>
+    val min = if (first > second) second else first
+    findMin(insert(second, insert(first, empty))) == min
+  }
+
+  property("findingAndRemovingTheMinimumOfTheHeapShouldReturnSortedListOfElements") = forAll { (heap: H) =>
+    def elements(h: H, acc: List[A]): List[A] = {
+      if (isEmpty(h)) acc
+      else {
+        val min = findMin(h)
+        elements(deleteMin(h), min :: acc)
+      }
+    }
+
+    val all = elements(heap, List.empty)
+    all.sorted == all.reverse
+  }
+
+  property("meldingTwoHeapsShouldResultInAHeapWithCorrectMinimum") = forAll { (first: H, second: H) =>
+    def minWithFallback(first: H, second: H) = if (!isEmpty(first)) findMin(first) else findMin(second)
+
+    if (isEmpty(first) && isEmpty(second)) isEmpty(meld(first, second))
+    else {
+      val firstMin = minWithFallback(first, second)
+      val secondMin = minWithFallback(second, first)
+
+      val min = if (firstMin < secondMin) firstMin else secondMin
+
+      findMin(meld(first, second)) == min
+    }
+  }
 }
